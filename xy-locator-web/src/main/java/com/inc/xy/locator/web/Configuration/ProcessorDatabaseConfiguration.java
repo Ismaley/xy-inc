09a9d94ab2e9d5087a3676de8a91f9d508df5c27
Ismaley Viana <ismaley.viana@gmail.com>
2018-03-21 14:25:43 -0300

    liquibase and changelog

---
+++ xy-locator-web/src/main/java/com/inc/xy/locator/web/Configuration/ProcessorDatabaseConfiguration.java
@@ -0,0 +1,28 @@
+package com.inc.xy.locator.web.Configuration;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+public class ProcessorDatabaseConfiguration {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    public SpringLiquibase liquibase() throws SQLException {
+
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath*:db/changelog-master.xml");
+        return liquibase;
+    }
+
+}
+
+
