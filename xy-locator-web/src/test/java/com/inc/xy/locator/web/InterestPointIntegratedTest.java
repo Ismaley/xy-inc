86fac691a9706f2a3de284b8d2b98eefbe5c1de3
Ismaley Viana <ismaley.viana@gmail.com>
2018-03-23 14:06:18 -0300

    integrated tests

--- xy-locator-web/src/test/java/com/inc/xy/locator/web/InterestPointIntegratedTest.java
+++ xy-locator-web/src/test/java/com/inc/xy/locator/web/InterestPointIntegratedTest.java
@@ -2,8 +2,11 @@ package com.inc.xy.locator.web;
 
 import com.google.gson.Gson;
 import com.inc.xy.locator.api.to.InterestPointTO;
+import com.inc.xy.locator.model.InterestPoint;
 import com.inc.xy.locator.repository.InterestPointsRepository;
+import com.inc.xy.locator.service.exceptions.BusinessException;
 import com.inc.xy.locator.web.application.XyLocatorApplication;
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ErrorMessages;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,10 +16,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import static com.inc.xy.locator.service.exceptions.BusinessException.ErrorCode.*;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -53,8 +60,7 @@ public class InterestPointIntegratedTest {
                 .andExpect(jsonPath("$.id", notNullValue()))
                 .andExpect(jsonPath("$.pointName", is(point.getPointName())))
                 .andExpect(jsonPath("$.latitude", is(point.getLatitude())))
-                .andExpect(jsonPath("$.longitude", is(point.getLongitude())))
-                .andDo(MockMvcResultHandlers.print());
+                .andExpect(jsonPath("$.longitude", is(point.getLongitude())));
     }
 
     @Test
@@ -66,7 +72,131 @@ public class InterestPointIntegratedTest {
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest())
-                .andDo(MockMvcResultHandlers.print());
+                .andExpect(jsonPath("$.errorCode", is(INVALID_NAME.code)))
+                .andExpect(jsonPath("$.message", is(INVALID_NAME.message)))
+                .andExpect(jsonPath("$.pathInfo", is(PATH)));
+    }
+
+    @Test
+    public void shouldNotCreateInterestPointWithDuplicatedName() throws Exception {
+        repository.save(InterestPoint.builder().pointName("point name").latitude(32).longitude(55).build());
+
+        InterestPointTO point = buildPoint("point name", 22, 22);
+
+        mockMvc.perform(post(PATH)
+                .content(gson.toJson(point))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorCode", is(DUPLICATED_NAME.code)))
+                .andExpect(jsonPath("$.message", is(DUPLICATED_NAME.message)))
+                .andExpect(jsonPath("$.pathInfo", is(PATH)));
+    }
+
+    @Test
+    public void shouldNotCreateInterestPointWithDuplicatedCoordinates() throws Exception {
+        repository.save(InterestPoint.builder().pointName("point name").latitude(22).longitude(22).build());
+
+        InterestPointTO point = buildPoint("another point name", 22, 22);
+
+        mockMvc.perform(post(PATH)
+                .content(gson.toJson(point))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorCode", is(DUPLICATED_COORDINATES.code)))
+                .andExpect(jsonPath("$.message", is(DUPLICATED_COORDINATES.message)))
+                .andExpect(jsonPath("$.pathInfo", is(PATH)));
+    }
+
+    @Test
+    public void shouldNotCreateInterestPointWithInvalidCoordinates() throws Exception {
+        InterestPointTO point = buildPoint("another point name", null, 22);
+
+        mockMvc.perform(post(PATH)
+                .content(gson.toJson(point))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorCode", is(INVALID_COORDINATES.code)))
+                .andExpect(jsonPath("$.message", is(INVALID_COORDINATES.message)))
+                .andExpect(jsonPath("$.pathInfo", is(PATH)));
+    }
+
+    @Test
+    public void shouldFindAllPoints() throws Exception {
+        repository.save(InterestPoint.builder().pointName("point name").latitude(22).longitude(22).build());
+        repository.save(InterestPoint.builder().pointName("another point name").latitude(35).longitude(22).build());
+
+        mockMvc.perform(get(PATH)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$.[0].pointName", is("point name")))
+                .andExpect(jsonPath("$.[1].pointName", is("another point name")));
+    }
+
+    @Test
+    public void shouldReturnEmptyListIfNoPointsAreSaved() throws Exception {
+
+        mockMvc.perform(get(PATH)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    public void shouldFindPointsByProximity() throws Exception {
+        repository.save(InterestPoint.builder().pointName("point name").latitude(22).longitude(22).build());
+        repository.save(InterestPoint.builder().pointName("other point name").latitude(17).longitude(15).build());
+        repository.save(InterestPoint.builder().pointName("another point name").latitude(23).longitude(50).build());
+
+        String path = String.format("%s/%s", PATH, "byProximity");
+
+        mockMvc.perform(get(path)
+                .accept(MediaType.APPLICATION_JSON)
+                .param("latitude", "15")
+                .param("longitude", "15")
+                .param("radius", "7")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$.[0].pointName", is("point name")))
+                .andExpect(jsonPath("$.[1].pointName", is("other point name")));
+    }
+
+    @Test
+    public void shouldReturnEmptyListIfThereAreNoPointsNearby() throws Exception {
+        repository.save(InterestPoint.builder().pointName("another point name").latitude(23).longitude(50).build());
+
+        String path = String.format("%s/%s", PATH, "byProximity");
+
+        mockMvc.perform(get(path)
+                .accept(MediaType.APPLICATION_JSON)
+                .param("latitude", "15")
+                .param("longitude", "15")
+                .param("radius", "7")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    public void shouldNotFindPointsByProximityIfSearchParamIsInvalid() throws Exception {
+        repository.save(InterestPoint.builder().pointName("another point name").latitude(23).longitude(50).build());
+
+        String path = String.format("%s/%s", PATH, "byProximity");
+
+        mockMvc.perform(get(path)
+                .accept(MediaType.APPLICATION_JSON)
+                .param("longitude", "15")
+                .param("radius", "7")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.errorCode", is(INVALID_SEARCH_PARAM.code)))
+                .andExpect(jsonPath("$.message", is(INVALID_SEARCH_PARAM.message)))
+                .andExpect(jsonPath("$.pathInfo", is(path)));
     }
 
     private InterestPointTO buildPoint(String name, Integer latitude, Integer longitude) {
