3810b08f4c95cf68b06913aecff25185b6ff2485
Ismaley Viana <ismaley.viana@gmail.com>
2018-03-25 01:23:24 -0300

    documentation done

--- xy-locator-web/src/test/java/com/inc/xy/locator/web/InterestPointIntegratedTest.java
+++ xy-locator-web/src/test/java/com/inc/xy/locator/web/InterestPointIntegratedTest.java
@@ -1,58 +1,73 @@
 package com.inc.xy.locator.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.inc.xy.locator.api.to.InterestPointTO;
 import com.inc.xy.locator.model.InterestPoint;
 import com.inc.xy.locator.repository.InterestPointsRepository;
-import com.inc.xy.locator.service.exceptions.BusinessException;
 import com.inc.xy.locator.web.application.XyLocatorApplication;
-import jdk.nashorn.internal.runtime.regexp.joni.exception.ErrorMessages;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static com.inc.xy.locator.service.exceptions.BusinessException.ErrorCode.*;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = XyLocatorApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class InterestPointIntegratedTest {
 
     @Autowired
-    MockMvc mockMvc;
+    private WebApplicationContext context;
 
     @Autowired
     InterestPointsRepository repository;
 
-    private static final String PATH = "/interest-points";
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    MockMvc mockMvc;
+
+    private static final String PATH = "/interest-points";
     private Gson gson = new Gson();
 
     @Before
     public void setUp() {
         repository.deleteAll();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(documentationConfiguration(this.restDocumentation))
+                .alwaysDo(document("{method-name}",
+                        preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint())))
+                .build();
     }
 
     @Test
     public void shouldCreateInterestPoint() throws Exception {
         InterestPointTO point = buildPoint("point name", 22, 22);
 
-        mockMvc.perform(post(PATH)
+        this.mockMvc.perform(post(PATH)
             .content(gson.toJson(point))
             .accept(MediaType.APPLICATION_JSON)
             .contentType(MediaType.APPLICATION_JSON))
@@ -67,7 +82,7 @@ public class InterestPointIntegratedTest {
     public void shouldNotCreateInterestPointWithEmptyName() throws Exception {
         InterestPointTO point = buildPoint("", 22, 22);
 
-        mockMvc.perform(post(PATH)
+        this.mockMvc.perform(post(PATH)
                 .content(gson.toJson(point))
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON))
@@ -83,7 +98,7 @@ public class InterestPointIntegratedTest {
 
         InterestPointTO point = buildPoint("point name", 22, 22);
 
-        mockMvc.perform(post(PATH)
+        this.mockMvc.perform(post(PATH)
                 .content(gson.toJson(point))
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON))
@@ -97,7 +112,7 @@ public class InterestPointIntegratedTest {
     public void shouldNotCreateInterestPointWithInvalidCoordinates() throws Exception {
         InterestPointTO point = buildPoint("another point name", null, 22);
 
-        mockMvc.perform(post(PATH)
+        this.mockMvc.perform(post(PATH)
                 .content(gson.toJson(point))
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON))
@@ -112,7 +127,7 @@ public class InterestPointIntegratedTest {
         repository.save(InterestPoint.builder().pointName("point name").latitude(22).longitude(22).build());
         repository.save(InterestPoint.builder().pointName("another point name").latitude(35).longitude(22).build());
 
-        mockMvc.perform(get(PATH)
+        this.mockMvc.perform(get(PATH)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -124,7 +139,7 @@ public class InterestPointIntegratedTest {
     @Test
     public void shouldReturnEmptyListIfNoPointsAreSaved() throws Exception {
 
-        mockMvc.perform(get(PATH)
+        this.mockMvc.perform(get(PATH)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -142,7 +157,7 @@ public class InterestPointIntegratedTest {
 
         String path = String.format("%s/%s", PATH, "byProximity");
 
-        mockMvc.perform(get(path)
+        this.mockMvc.perform(get(path)
                 .accept(MediaType.APPLICATION_JSON)
                 .param("latitude", "2")
                 .param("longitude", "3")
@@ -159,15 +174,15 @@ public class InterestPointIntegratedTest {
 
     @Test
     public void shouldReturnEmptyListIfThereAreNoPointsNearby() throws Exception {
-        repository.save(InterestPoint.builder().pointName("another point name").latitude(23).longitude(50).build());
+        repository.save(InterestPoint.builder().pointName("bakery").latitude(4).longitude(1).build());
 
         String path = String.format("%s/%s", PATH, "byProximity");
 
-        mockMvc.perform(get(path)
+        this.mockMvc.perform(get(path)
                 .accept(MediaType.APPLICATION_JSON)
-                .param("latitude", "15")
-                .param("longitude", "15")
-                .param("radius", "7")
+                .param("latitude", "2")
+                .param("longitude", "3")
+                .param("radius", "1")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(0)));
@@ -175,11 +190,11 @@ public class InterestPointIntegratedTest {
 
     @Test
     public void shouldNotFindPointsByProximityIfSearchParamIsInvalid() throws Exception {
-        repository.save(InterestPoint.builder().pointName("another point name").latitude(23).longitude(50).build());
+        repository.save(InterestPoint.builder().pointName("bakery").latitude(4).longitude(1).build());
 
         String path = String.format("%s/%s", PATH, "byProximity");
 
-        mockMvc.perform(get(path)
+        this.mockMvc.perform(get(path)
                 .accept(MediaType.APPLICATION_JSON)
                 .param("longitude", "15")
                 .param("radius", "7")
